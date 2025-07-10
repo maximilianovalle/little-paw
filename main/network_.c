4a2a149ef4de20e3e7ac5c4b273c80c0cfc8ae6f
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-07-09 23:12:41 -0500

    Finish network functions

--- main/network_.c
+++ main/network_.c
@@ -92,20 +92,116 @@ static void wifi_event_cb(void *arg, esp_event_base_t event_base, int32t event_i
 // Sets up everything needed before WiFi can work...
 esp_err_t network_init(void) {
     // NVS (Non-Volatile Storage): WiFi credentials, etc.
+    esp_err_t ret = nvs_flash_init();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    s_wifi_event_group = xEventGroupCreate();
+    
     // TCP/IP Stack: lets ESP32 talk over network
+    ret = esp_netif_init();
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize TCP/IP network stack");
+        return ret;
+    }
+
     // Event Loop: required for reacting to WiFi connection/disconnection
-    // WiFi Interface: creates a WiFi station interface
+    ret = esp_event_loop_create_default();
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to create default event loop");
+        return ret;
+    }
+
     // Registers Callback: two event handler functions defined for IP events and WiFi events
+    ret = esp_wifi_set_default_wifi_sta_handlers();
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to set default handlers");
+        return ret;
+    }
+
+    // WiFi Interface: creates a WiFi station interface
+    network_netif = esp_netif_create_default_wifi_sta();
+
+    if (network_netif == NULL) {
+        ESP_LOGE(TAG, "Failed to create default WiFi STA interface");
+        return ESP_FAIL;
+    }
+
+    // WiFi Stack Configuration Parameters
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &Wifi_event_cb, NULL, &wifi_event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &ip_event_cb, NULL, &ip_event_handler));
+    
+    return ret;
 }
 
 esp_err_t network_connect(char *wifi_ssid, char *wifi_password) {
+    // Stores SSID + Password in a "Config" structure
+    wifi_config_t wifi_config = {
+        .sta = {
+            .threshold.authmode = WIFI_AUTHMODE,
+        },
+    };
 
+    strncpy((char *)wifi_config.sta.ssid, wifi_ssid, sizeof(wifi_config.sta.ssid));
+    strncpy((char *)wifi_config.sta.password, wifi_password, sizeof(wifi_config.sta.password));
+
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));             // Default: WIFI_PS_MIN_MODEM
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));    // Default: WIFI_STORAGE_FLASH
+
+    // Sets ESP32 in WiFi Station mode
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    // Applies Config + Starts WiFi
+    ESP_LOGI(TAG, "Connecting to WiFi network: %s", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "Connected to WiFi network: %s", wifi_config.sta.ssid);
+        return ESP_OK;
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGE(TAG, "Failed to connect to WiFi network: %s", wifi_config.sta.ssid);
+        return ESP_FAIL;
+    }
+
+    ESP_LOGE(TAG, "Unexpected WiFi error");
+    return ESP_FAIL;
 }
 
+// Disconnects ESP32 from WiFi + Deletes Event Group
 esp_err_t network_disconnect(void) {
+    if (s_wifi_event_group) {
+        vEventGroupDelete(s_wifi_event_group);
+    }
 
+    return esp_wifi_disconnect();
 }
 
+// Shuts Down WiFi Driver + Unregisters Event Handlers (clears up memory)
 esp_err_t network_deinit(void) {
+    esp_err_t ret = esp_wifi_stop();
 
+    if (ret == ESP_ERR_WIFI_NOT_INTI) {
+        ESP_LOGE(TAG, "WiFi stack not initialized");
+        return ret;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+    ESP_ERROR_CHECK(esp_wifi_clear_default_wifi_driver_and_handlers(network_netif));
+    esp_netif_destroy(network_netif);
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, ESP_EVENT_ANY_ID, ip_event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler));
+
+    return ESP_OK;
 }
